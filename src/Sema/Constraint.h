4eb0ca5a5eab921b16504f6855e40d5703e2eff2
Zach Wolfe <zachrwolfe@me.com>
2018-03-07 01:10:15 -0600

    Add a type constraint struct.

---
+++ src/Sema/Constraint.h
@@ -0,0 +1,51 @@
+//  Copyright © 2018 Zach Wolfe. All rights reserved.
+
+#pragma once
+
+#include <vector>
+#include "boost/variant.hpp"
+
+struct Constraint {
+    struct EqualConstraint {
+        Type lhs, rhs;
+    };
+    struct BindOverloadConstraint {
+        Type ty;
+        std::shared_ptr<Decl> decl;
+    };
+    struct DisjunctionConstraint {
+        std::vector<Constraint> constraints;
+    };
+    struct ConjunctionConstraint {
+        std::vector<Constraint> constraints;
+    };
+    struct IntegerLiteralConstraint {
+        Type ty;
+    };
+    struct DecimalLiteralConstraint {
+        Type ty;
+    };
+    typedef boost::variant<EqualConstraint, BindOverloadConstraint, DisjunctionConstraint,
+                           ConjunctionConstraint, IntegerLiteralConstraint, DecimalLiteralConstraint>
+        Data;
+private:
+    Data data;
+    Constraint(Data data) : data(data) {}
+public:
+    static Constraint Equal(Type lhs, Type rhs) { return Constraint(EqualConstraint { lhs, rhs }); }
+    static Constraint Disjunction(std::vector<Constraint> constraints) {
+        return Constraint(DisjunctionConstraint { constraints });
+    }
+    static Constraint Conjunction(std::vector<Constraint> constraints) {
+        return Constraint(ConjunctionConstraint { constraints });
+    }
+
+    static Constraint BindOverload(Type ty, std::shared_ptr<Decl> decl) {
+        return Constraint(BindOverloadConstraint { ty, decl });
+    }
+
+    static Constraint IntegerLiteral(Type ty) { return Constraint(IntegerLiteralConstraint { ty }); }
+    static Constraint DecimalLiteral(Type ty) { return Constraint(DecimalLiteralConstraint { ty }); }
+
+    Data getData() const { return data; }
+};
