b7d3d1d34a62802764d658bd10e31b04ee5c77a6
Zach <zachrwolfe@me.com>
2019-03-23 20:28:42 -0500

    Begin refactoring MIR and the type checker to work in a more conventional Hindley-Milner type constraint system sort of way

--- meda/src/mir.rs
+++ meda/src/mir.rs
@@ -1,18 +1,18 @@
-#[derive(Debug)]
+use std::fmt;
+
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum IntWidth {
     W8, W16, W32, W64,
 }
 
-#[derive(Debug)]
+#[derive(Clone, PartialEq, Eq, Debug)]
 pub enum FloatWidth {
     W32, W64,
 }
 
-#[derive(Debug)]
-pub enum TypeKind {
+#[derive(Clone, PartialEq, Eq)]
+pub enum Type {
     Error,
-    IntVar,
-    FloatVar,
     Int {
         width: IntWidth,
         is_signed: bool,
@@ -22,33 +22,37 @@ pub enum TypeKind {
     Void,
 }
 
-pub type TypeVarID = usize;
-
-#[derive(Debug)]
-pub struct Type {
-    pub kind: TypeKind,
-    pub id: Option<TypeVarID>,
-}
-
-pub struct TypeVar {
-    pub locations: Vec<usize>,
-    pub ty: Type,
-}
-
-impl TypeVar {
-    pub fn new(location: usize, ty: Type) -> Self {
-        Self {
-            locations: vec![location],
-            ty,
-        }
+impl Default for Type {
+    fn default() -> Self {
+        Type::Error
     }
 }
 
-impl Default for Type {
-    fn default() -> Self {
-        Self {
-            kind: TypeKind::Error,
-            id: None,
+impl fmt::Debug for Type {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            Type::Error => write!(f, "<ERROR>"),
+            Type::Int { width, is_signed } => write!(
+                f,
+                "{}{}", 
+                if *is_signed { 'i' } else { 'u' },
+                match width {
+                    IntWidth::W8 => 8,
+                    IntWidth::W16 => 16,
+                    IntWidth::W32 => 32,
+                    IntWidth::W64 => 64,
+                }
+            ),
+            Type::Float(width) => write!(
+                f,
+                "{}",
+                match width {
+                    FloatWidth::W32 => 32,
+                    FloatWidth::W64 => 64,
+                }
+            ),
+            Type::Bool => write!(f, "bool"),
+            Type::Void => write!(f, "void"),
         }
     }
 }
--- meda/src/type_checker.rs
+++ meda/src/type_checker.rs
@@ -1,26 +1,47 @@
 use crate::error::Error;
-use crate::hir::{Program, ExprKind};
-use crate::mir::{Type, TypeVar, TypeKind, IntWidth, FloatWidth};
+use crate::hir::{Program, ExprKind, BinOp};
+use crate::mir::{Type, IntWidth, FloatWidth};
 
-const DEFAULT_INT_TY: Type = Type {
-    kind: TypeKind::Int {
-        width: IntWidth::W64,
-        is_signed: true,
-    },
-    id: None,
-};
-
-const DEFAULT_FLOAT_TY: Type = Type {
-    kind: TypeKind::Float(FloatWidth::W64),
-    id: None,
-};
-
-struct TypeChecker {
-    prog: Program,
-    types: Vec<Type>,
-    type_vars: Vec<TypeVar>,
+struct Overload {
+    param_tys: Vec<Type>,
+    ret_ty: Type,
 }
 
+struct OverloadConstraints {
+    param_tys: Vec<usize>,
+    ret_ty: usize,
+    viable_overloads: Vec<Overload>,
+}
+
+enum TypeConstraint {
+    IsIntLit,
+    IsDecLit,
+    OneOf(Vec<Type>),
+    ConvertibleTo(Type),
+    Is(Type),
+}
+
+const DEFAULT_INT_TY: Type = Type::Int {
+    width: IntWidth::W64,
+    is_signed: true,
+};
+
+const DEFAULT_FLOAT_TY: Type = Type::Float(FloatWidth::W64);
+
+struct TypeChecker {
+    /// The input HIR program
+    prog: Program,
+    /// The type of each expression
+    types: Vec<Type>,
+    /// The constraints on each expression's type
+    constraints: Vec<Vec<TypeConstraint>>,
+    /// The constraints on each function call or operator expression's overload choices
+    overload_constraints: Vec<OverloadConstraints>,
+    /// The selected overload for each function call or operator expression
+    overloads: Vec<Overload>,
+}
+
+/*
 pub fn type_check(prog: Program) -> Vec<Error> {
     let mut tc = TypeChecker {
         prog,
@@ -28,7 +49,7 @@ pub fn type_check(prog: Program) -> Vec<Error> {
         type_vars: Vec::new(),
     };
     let mut errs = Vec::new();
-    tc.type_vars.resize_with(self.prog.num_expressions, Default::default);
+    tc.types.resize_with(tc.prog.num_expressions, Default::default);
 
     for level in tc.prog.expressions {
         for expr in level {
@@ -43,20 +64,39 @@ pub fn type_check(prog: Program) -> Vec<Error> {
                 },
                 DecLit => {
                     tc.types[expr.id] = Type {
-                        kind: TypeKind::Float,
+                        kind: TypeKind::FloatVar,
                         id: Some(tc.type_vars.len()),
                     };
                     tc.type_vars.push(TypeVar::new(expr.id, DEFAULT_FLOAT_TY));
                 },
                 BinOp { op, lhs, rhs } => {
-                    use BinOp::*;
-                    let (lhsTy, rhsTy) = 
-                    match op {
-                        Mult => match (&tc.types)
+                    use crate::hir::BinOp::*;
+                    assert_ne!(lhs, rhs);
+                    assert!(lhs < tc.types.len() && rhs < tc.types.len());
+                    let buf = tc.types.as_mut_ptr();
+                    // This is safe because:
+                    //     - we asserted above that lhs and rhs are both in range
+                    //     - we asserted above that lhs are rhs are different indices
+                    //     - we know that tc.types outlives the scope of this function, and 
+                    //       will never grow
+                    // TODO: It would be nice to refactor the code to not require this pattern
+                    let (lhs_ty, rhs_ty) = unsafe { (&mut *buf.offset(lhs as isize), &*buf.offset(rhs as isize)) };
+                    macro_rules! both {
+                        
                     }
+                    let ty = match op {
+                        Mult => match (&lhs_ty.kind, &rhs_ty.kind) {
+                            both!(TypeKind::Int { .. })
+                            both!(TypeKind::Float(_))
+                            //both!(TypeKind::IntVar) => 
+                            _ => Type::default(),
+                        },
+                        _ => Type::default(),
+                    };
                 }
             }
         }
     }
     errs
 }
+*/
