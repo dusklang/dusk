a048068854a0b27967d2fad388bdd16061fceed2
Zach Wolfe <zachrwolfe@me.com>
2019-08-24 15:10:56 -0500

    Print tir, typechecking and hir errors before generating mir

--- src/main.rs
+++ src/main.rs
@@ -49,16 +49,16 @@ fn main() {
     }).unwrap();
     errs.extend(mid_errs);
 
-    let mir = mir::Program::build(&hir, &tc, arch::Arch::X86_64);
-
     for err in &errs { err.report(&file); }
     if !errs.is_empty() {
         println!("\n\u{001B}[31mcompilation failed due to previous {} errors\u{001B}[0m", errs.len());
-    } else {
-        let mut interpreter = Interpreter::new(&mir);
-        let main = mir.comp_decls.iter()
-            .position(|func| &*func.name == "main" && func.ret_ty == Type::Void && func.num_parameters() == 0)
-            .expect("Couldn't find main function with no parameters and a return type of void!");
-        interpreter.call(Idx::new(main), Vec::new());
+        return;
     }
+
+    let mir = mir::Program::build(&hir, &tc, arch::Arch::X86_64);
+    let mut interpreter = Interpreter::new(&mir);
+    let main = mir.comp_decls.iter()
+        .position(|func| &*func.name == "main" && func.ret_ty == Type::Void && func.num_parameters() == 0)
+        .expect("Couldn't find main function with no parameters and a return type of void!");
+    interpreter.call(Idx::new(main), Vec::new());
 }
