3f50a775b4e0eb73ec59aae1f336323e5ce1bda0
Zach Wolfe <zachrwolfe@me.com>
2020-02-17 04:46:49 -0600

    Add data to graph for finding weak components

--- src/tir/graph.rs
+++ src/tir/graph.rs
@@ -14,19 +14,28 @@ pub enum ItemId {
     Decl(DeclId),
 }
 
-pub trait Item {
-    fn adjacent(self, graph: &mut Graph) -> &mut Vec<ItemId>;
+pub trait Item: Copy + Into<ItemId> {
+    fn dependees(self, graph: &mut Graph) -> &mut Vec<ItemId>;
+    fn dependers(self, graph: &mut Graph) -> &mut Vec<ItemId>;
 }
 
 impl Item for ExprId {
-    fn adjacent(self, graph: &mut Graph) -> &mut Vec<ItemId> {
-        &mut graph.expr_adjacent[self]
+    fn dependees(self, graph: &mut Graph) -> &mut Vec<ItemId> {
+        &mut graph.expr_dependees[self]
+    }
+
+    fn dependers(self, graph: &mut Graph) -> &mut Vec<ItemId> {
+        &mut graph.expr_dependers[self]
     }
 }
 
 impl Item for DeclId {
-    fn adjacent(self, graph: &mut Graph) -> &mut Vec<ItemId> {
-        &mut graph.decl_adjacent[self]
+    fn dependees(self, graph: &mut Graph) -> &mut Vec<ItemId> {
+        &mut graph.decl_dependees[self]
+    }
+
+    fn dependers(self, graph: &mut Graph) -> &mut Vec<ItemId> {
+        &mut graph.decl_dependers[self]
     }
 }
 
@@ -43,19 +52,45 @@ impl From<DeclId> for ItemId {
 }
 
 pub struct Graph {
-    // TODO: consider putting ExprIds and DeclIds into separate arrays, or one sorted one, rather than all together in an enum
-    expr_adjacent: IdxVec<Vec<ItemId>, ExprId>,
-    decl_adjacent: IdxVec<Vec<ItemId>, DeclId>,
+    // TODO: consider putting ExprIds and DeclIds into separate arrays, or one sorted one, rather than all together using an enum
+    expr_dependees: IdxVec<Vec<ItemId>, ExprId>,
+    decl_dependees: IdxVec<Vec<ItemId>, DeclId>,
+
+    // Used exclusively for finding connected components
+    expr_dependers: IdxVec<Vec<ItemId>, ExprId>,
+    decl_dependers: IdxVec<Vec<ItemId>, DeclId>,
+
+    components: Vec<Vec<ItemId>>,
+}
+
+struct SplitState {
+    // TODO: bool array == gross
+    expr_visited: IdxVec<bool, ExprId>,
+    decl_visited: IdxVec<bool, DeclId>,
 }
 
 impl Graph {
-    pub fn add_type1_dep(&mut self, a: impl Item, b: impl Into<ItemId>) {
-        let b = b.into();
+    pub fn add_type1_dep(&mut self, a: impl Item, b: impl Item) {
+        let a_val = a.into();
+        let b_val = b.into();
         // TODO: remove this HACK to prevent type 1 dependencies on the void expression
-        if let ItemId::Expr(expr) = b {
+        if let ItemId::Expr(expr) = b_val {
             if expr.idx() == 0 { return; }
         }
-        a.adjacent(self).push(b);
+        a.dependees(self).push(b_val);
+        b.dependers(self).push(a_val);
+    }
+
+    // Find the weak components of the graph
+    pub fn split_graph(&mut self) {
+        let mut expr_visited = IdxVec::new();
+        expr_visited.resize_with(self.expr_dependees.len(), || false);
+        let mut decl_visited = IdxVec::new();
+        decl_visited.resize_with(self.decl_dependees.len(), || false);
+        let mut state = SplitState {
+            expr_visited, decl_visited
+        };
+        
     }
 }
 
@@ -71,12 +106,17 @@ impl ItemId {
 
 impl Driver {
     pub fn create_graph(&self) -> Graph {
-        let mut expr_adjacent = IdxVec::new();
-        expr_adjacent.resize_with(self.hir.exprs.len(), || Vec::new());
-        let mut decl_adjacent = IdxVec::new();
-        decl_adjacent.resize_with(self.hir.decls.len(), || Vec::new());
+        let mut expr_dependees = IdxVec::new();
+        expr_dependees.resize_with(self.hir.exprs.len(), || Vec::new());
+        let mut decl_dependees = IdxVec::new();
+        decl_dependees.resize_with(self.hir.decls.len(), || Vec::new());
 
-        Graph { expr_adjacent, decl_adjacent }
+        let mut expr_dependers = IdxVec::new();
+        expr_dependers.resize_with(self.hir.exprs.len(), || Vec::new());
+        let mut decl_dependers = IdxVec::new();
+        decl_dependers.resize_with(self.hir.decls.len(), || Vec::new());
+
+        Graph { expr_dependees, decl_dependees, expr_dependers, decl_dependers, components: Vec::new() }
     }
 
     // Prints graph in Graphviz format, then opens a web browser to display the results.
@@ -88,9 +128,9 @@ impl Driver {
         let mut w = File::create("tmp/tc_graph.gv")?;
         writeln!(w, "digraph G {{")?;
 
-        for i in 0..graph.expr_adjacent.len() {
+        for i in 0..graph.expr_dependees.len() {
             let a = ExprId::new(i);
-            for &b in &graph.expr_adjacent[a] {
+            for &b in &graph.expr_dependees[a] {
                 write!(w, "    ")?;
                 write!(w, "expr{}", i)?;
                 write!(w, " -> ")?;
@@ -98,9 +138,9 @@ impl Driver {
                 writeln!(w, ";")?;
             }
         }
-        for i in 0..graph.decl_adjacent.len() {
+        for i in 0..graph.decl_dependees.len() {
             let a = DeclId::new(i);
-            for &b in &graph.decl_adjacent[a] {
+            for &b in &graph.decl_dependees[a] {
                 write!(w, "    ")?;
                 write!(w, "decl{}", i)?;
                 write!(w, " -> ")?;
