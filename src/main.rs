179652d37bf14374b61a4b136aca84027a836583
Zach Wolfe <zachrwolfe@me.com>
2020-09-02 20:19:53 -0500

    Parse some basic CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use clap::Clap;
+use std::fs;
+use std::path::PathBuf;
+
 mod dep_vec;
 #[macro_use]
 mod index_vec;
@@ -16,8 +20,6 @@ mod driver;
 mod type_checker;
 mod interpreter;
 
-use std::fs;
-
 use arch::Arch;
 use interpreter::InterpMode;
 use index_vec::Idx;
@@ -25,7 +27,53 @@ use ty::Type;
 use mir::FunctionRef;
 use driver::Driver;
 
+
+#[derive(Clap, Debug)]
+enum TirGraphOutput {
+    None,
+    Items,
+    Components,
+    Units,
+}
+
+#[derive(Clap, Debug)]
+enum StopPhase {
+    Lexing,
+    Parsing,
+    Tir,
+    TypeChecking,
+    Mir,
+    Interpretation,
+}
+
+#[derive(Clap, Debug)]
+#[clap(name = "meda")]
+struct Opt {
+    /// Output per-level typechecking diffs
+    #[clap(short="d", long)]
+    output_tc_diff: bool,
+
+    /// The mode for displaying the TIR graph
+    #[clap(arg_enum, short="g", long, default_value="none", case_insensitive = true)]
+    tir_output: TirGraphOutput,
+
+    /// Output MIR in textual format
+    #[clap(short="m", long)]
+    output_mir: bool,
+
+    /// The phase to stop the compiler at
+    #[clap(arg_enum, short="s", long, default_value="interpretation", case_insensitive = true)]
+    stop_phase: StopPhase,
+
+    /// Input file
+    #[clap(parse(from_os_str))]
+    input: PathBuf,
+}
+
 fn main() {
+    let opt = Opt::parse();
+    println!("options: {:#?}", opt);
+
     let contents = fs::read_to_string("HelloWorld.meda")
         .expect("unable to read input file");
     let file = source_info::SourceFile::new(
