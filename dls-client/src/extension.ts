1b458fda07a71a2b675286b351eceac50712292b
Zach Wolfe <zachrwolfe@me.com>
2022-03-12 09:06:28 -0600

    Add DLS vscode extension

---
+++ dls-client/src/extension.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import { ExtensionContext } from 'vscode';
+import { TransportKind, LanguageClient } from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    const extension = process.platform == 'win32' ? '.exe' : '';
+    // TODO: support release mode
+    const serverPath = context.asAbsolutePath(
+        path.join('..', 'target', 'debug', `dls${extension}`)
+    );
+
+    client = new LanguageClient(
+        'dls',
+        'Dusk Language Server',
+        {
+            module: serverPath,
+            transport: TransportKind.stdio,
+        },
+        {
+            documentSelector: [{ scheme: 'file', language: 'dusk'}],
+        }
+    );
+    client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if(!client) return undefined;
+    return client.stop();
+}
